fce157bbfb617383aa49abd233b985ebb2aa4980
DEBASHIS559 <debashisganguly321@gmail.com>
2024-02-28 14:52:25 +0530

    @GeneratedValue added in the id for the user

--- src/main/java/com/ff/rentalApp/entity/User.java
+++ src/main/java/com/ff/rentalApp/entity/User.java
@@ -3,6 +3,8 @@ package com.ff.rentalApp.entity;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
@@ -15,6 +17,7 @@ import lombok.Data;
 public class User {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
 	private String userName;

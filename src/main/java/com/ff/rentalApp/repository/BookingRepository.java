f96ce1e6b5f6cd91795fd87fd853137aea673ed1
MdShayequeZiya <shayequeziya@outlook.com>
2024-02-28 14:30:33 +0530

    Booking entity and repository created

---
+++ src/main/java/com/ff/rentalApp/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.ff.rentalApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ff.rentalApp.entity.Booking;
+
+public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+}

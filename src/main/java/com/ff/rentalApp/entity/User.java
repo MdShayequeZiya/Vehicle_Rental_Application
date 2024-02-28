64160d782febd4cab5a7d7b7ce308d644f9cbe58
DEBASHIS559 <debashisganguly321@gmail.com>
2024-02-28 14:38:20 +0530

    User entity and repository created

--- src/main/java/com/ff/rentalApp/entity/User.java
+++ src/main/java/com/ff/rentalApp/entity/User.java
@@ -1,14 +1,34 @@
 package com.ff.rentalApp.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
 	
 	@Id
 	private int id;
-
+	
+	private String userName;
+	private String email;
+	private String password;
+	private double mobileNo;
+	private String userRole;
+	
+	@OneToMany
+	private List<Booking> listBooking;
+	
+	@OneToOne
+	private Address address;
+	
+	
+	
 }
